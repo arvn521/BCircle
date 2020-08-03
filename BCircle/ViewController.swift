c165765136060daaf4d64eb9c9d595171ecc6c1a
Arvin Nidadavolu <arvin.nid@gmail.com>
2020-08-03 13:26:31 -0700

    Initial Commit

---
+++ BCircle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BCircle
+//
+//  Created by Arvin Nidadavolu on 8/3/20.
+//  Copyright © 2020 Arvin Nidadavolu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
